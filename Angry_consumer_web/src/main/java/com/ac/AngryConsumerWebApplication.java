076d8e140b4995bc317d72632f2548c6ee507b68
Jbee <Jbee@AL01155703.local>
2016-10-17 11:53:29 +0900

    initial commit / index.html mark up

---
+++ Angry_consumer_web/src/main/java/com/ac/AngryConsumerWebApplication.java
@@ -0,0 +1,12 @@
+package com.ac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngryConsumerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngryConsumerWebApplication.class, args);
+	}
+}
