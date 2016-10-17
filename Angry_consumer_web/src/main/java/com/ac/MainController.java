8484fff15ee38988c4502f23191368e053d7c308
Jbee <Jbee@AL01155703.local>
2016-10-17 14:47:26 +0900

    MainController.java add

---
+++ Angry_consumer_web/src/main/java/com/ac/MainController.java
@@ -0,0 +1,15 @@
+package com.ac;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Jbee on 2016. 10. 17..
+ */
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainPage(){
+        return "index";
+    }
+}
