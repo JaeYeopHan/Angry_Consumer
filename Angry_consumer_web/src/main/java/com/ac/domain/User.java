e4e2fb7577c3d48de7c53d5f06302d69edeb57c0
Jbee <Jbee@AL01155703.local>
2016-11-21 14:18:25 +0900

    toString(), equals(), hashcode() override .

--- Angry_consumer_web/src/main/java/com/ac/domain/User.java
+++ Angry_consumer_web/src/main/java/com/ac/domain/User.java
@@ -44,10 +44,27 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id == user.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
     }
 
     public boolean matchPassword(String password) {
