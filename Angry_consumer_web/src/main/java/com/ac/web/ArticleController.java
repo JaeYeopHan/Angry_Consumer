fa208d6fcbba993aed92631053a638487cd051df
Jbee <Jbee@AL01155703.local>
2016-11-21 14:20:32 +0900

    create Article, getList method create .

--- Angry_consumer_web/src/main/java/com/ac/web/ArticleController.java
+++ Angry_consumer_web/src/main/java/com/ac/web/ArticleController.java
@@ -1,11 +1,16 @@
 package com.ac.web;
 
 import com.ac.domain.Article;
+import com.ac.domain.ArticleRepository;
+import com.ac.domain.User;
 import com.ac.util.HttpSessionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * Created by Jbee on 2016. 10. 19..
@@ -14,9 +19,12 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/articles")
 public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @GetMapping("")
-    public String listPage() {
+    public String listPage(Model model) {
+        model.addAttribute("articles", articleRepository.getArticleList());
         return "/article/article_list";
     }
 
@@ -30,23 +38,24 @@ public class ArticleController {
     }
 
     @PostMapping("/create")
-    public String articleCreate(@PathVariable  Article article) {
-
+    public String articleCreate(Article article) {
+        System.out.println(article);
+        articleRepository.articleInsert(article);
         return "redirect:/articles";
     }
 
-    @GetMapping("/detail")//detail에는 article의 id 값을 넣자
-    public String articleDetail() {
-        return "/article/article_detail";
-    }
-
-    @PutMapping("/{id}")
-    public String update() {
-        return "redirect:/articles";
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete() {
-        return "redirect:/articles";
-    }
+//    @GetMapping("/detail")//detail에는 article의 id 값을 넣자
+//    public String articleDetail() {
+//        return "/article/article_detail";
+//    }
+//
+//    @PutMapping("/{id}")
+//    public String update() {
+//        return "redirect:/articles";
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public String delete() {
+//        return "redirect:/articles";
+//    }
 }
