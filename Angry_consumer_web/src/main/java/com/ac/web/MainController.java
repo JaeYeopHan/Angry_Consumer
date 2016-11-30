5a28540afe9b12fa782eed57eac4817f70d766f2
Jbee <Jbee@AL01155703.local>
2016-11-30 15:21:48 +0900

    코드 세부 사항 수정 .

--- Angry_consumer_web/src/main/java/com/ac/web/MainController.java
+++ Angry_consumer_web/src/main/java/com/ac/web/MainController.java
@@ -5,17 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Jbee on 2016. 10. 17..
  */
 @Controller
+@RequestMapping("/")
 public class MainController {
 
     @Autowired
     private ArticleRepository articleRepository;
 
-    @GetMapping("/")
+    @GetMapping("")
     public String mainPage(Model model) {
         model.addAttribute("articles", articleRepository.getArticleListCountOfSix());
         return "index";
