3037532aca632b7870a44975d6d6d98295e160ab
Jbee <Jbee@AL01155703.local>
2016-11-30 17:16:37 +0900

    main controller update .

--- Angry_consumer_web/src/main/java/com/ac/web/MainController.java
+++ Angry_consumer_web/src/main/java/com/ac/web/MainController.java
@@ -1,11 +1,15 @@
 package com.ac.web;
 
+import com.ac.domain.Article;
 import com.ac.domain.ArticleRepository;
+import com.ac.domain.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by Jbee on 2016. 10. 17..
@@ -17,9 +21,21 @@ public class MainController {
     @Autowired
     private ArticleRepository articleRepository;
 
+    @Autowired
+    private ImageRepository imageRepository;
+
     @GetMapping("")
     public String mainPage(Model model) {
-        model.addAttribute("articles", articleRepository.getArticleListCountOfSix());
+
+        List<Article> articleList = articleRepository.getArticleListCountOfSix();
+
+        for(Article article : articleList) {
+            int imageId = article.getIdImage();
+            String imagePath = imageRepository.getArticleImagePathById(imageId);
+            article.setFilePath(imagePath);
+        }
+
+        model.addAttribute("articles", articleList);
         return "index";
     }
 
